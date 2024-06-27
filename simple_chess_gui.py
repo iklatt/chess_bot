a77acdec3b76460d02598ffcaab4ae269308e499
Isaiah Klatt <klatt093@umn.edu>
2024-06-27 17:55:59 -0500

    Added tkinter window with chess board.

---
+++ simple_chess_gui.py
@@ -0,0 +1,24 @@
+import tkinter
+
+#create and size window:
+window = tkinter.Tk()
+window.geometry("850x850")
+window.configure(background="gray")
+
+#create canvas:
+canvas = tkinter.Canvas(window, highlightbackground= "black", width=800, height=800)
+canvas.place(relx=.5, rely=.5, anchor=tkinter.CENTER)
+#create squares of board:
+for row in range(8):
+    for column in range(8):
+        if (row + column) % 2 == 0:
+            color="black"
+        else:
+            color = "white"
+        canvas.create_rectangle(row * 100 + 1, column * 100 + 1, (row + 1) * 100 + 1, (column + 1) * 100 + 1, 
+                                fill=color, outline = 'black')
+
+canvas.pack()
+
+
+window.mainloop()
