a251d2632923cfc9798007938dfd8a3fb2ded70c
Isaiah Klatt <klatt093@umn.edu>
2024-06-30 17:50:19 -0500

    Added functionality to setup position from FEN. Added function that can convert canvas coordinates to standard board location notation.

--- simple_chess_gui.py
+++ simple_chess_gui.py
@@ -1,6 +1,5 @@
 from tkinter import *
 from PIL import Image, ImageTk
-import drag_manager as dm
 
 # Create and size window:
 window = Tk()
@@ -8,7 +7,7 @@ window.geometry("850x850")
 window.configure(background="gray")
 
 # Label for testing:
-coords_label = Label(window, text='TESTING')
+coords_label = Label(window, text='TESTING TEXT')
 coords_label.pack()
 
 # Create canvas:
@@ -26,10 +25,56 @@ for row in range(8):
                                 fill=color)
 
 #Adding the pieces to the board:
-image = Image.open("piece_images\\black_queen.png").convert('RGBA')
-resized_image = image.resize((100, 100))
-photo_image = ImageTk.PhotoImage(resized_image)
-piece = canvas.create_image(150, 150, image=photo_image)
+
+# Dictionary to hold image references
+image_references = {}
+piece_dictionary = {
+    "K": "white_king.png",
+    "Q": "white_queen.png",
+    "R": "white_rook.png",
+    "B": "white_bishop.png",
+    "N": "white_knight.png",
+    "P": "white_pawn.png",
+    "k": "black_king.png",
+    "q": "black_queen.png",
+    "r": "black_rook.png",
+    "b": "black_bishop.png",
+    "n": "black_knight.png",
+    "p": "black_pawn.png"
+}
+def set_piece(fen_piece, xPos, yPos):
+    global img
+    file = "piece_images\\" + piece_dictionary[fen_piece]
+    img = ImageTk.PhotoImage( Image.open(file).convert('RGBA').resize((100, 100)) )
+    image_references[(xPos, yPos)] = img
+    canvas.create_image(xPos, yPos, image=img)
+
+
+def set_up_from_FEN(fen):
+    row = 0
+    col = 0
+    for char in fen:
+        if char == "/":
+            row += 1
+            col = 0
+        elif char == " ":
+            break
+        elif char.isdigit():
+            col += int(char)
+        else:
+            set_piece(char, col * 100 + 50, row * 100  + 50)
+            col += 1
+
+# Convert coordinates to chess notation for that square:
+def convert_board_location(xPos, yPos, boardIsFlipped):
+    x = xPos // 100
+    y = yPos // 100
+    if boardIsFlipped:
+        x = 7 - x
+        y += 1
+    else:
+        y = 8 - y
+    return chr(97 + x) + str(y)
 
 # Drag and Drop functionality:
 # This function chooses which piece is getting dragged.
@@ -39,21 +84,27 @@ def on_start(event):
     pass
 
 def move(event):
-    canvas.moveto(piece, event.x - 50, event.y - 50)
-    coords_label.config(text="Coordinates: " + str(event.x) + ", " + str(event.y))
+    # canvas.moveto(piece, event.x - 50, event.y - 50)
+    # coords_label.config(text="Coordinates: " + str(event.x) + ", " + str(event.y))
+    pass
 
 # This function ensures the piece is centered on the square that it was dropped onto.
-#TODO: Need these squares to be valid, ie can't be out of bounds nor can it be an illegal move
+#TODO: Need these squares to be valid, ie can't be out of bounds nor can it be an illegal move.
+#      If an illegal move is made, return the piece to its original location
 def on_drop(event):
-    coords = canvas.coords(piece)
-    x = (coords[0] // 100) * 100
-    y = (coords[1] // 100) * 100
-    canvas.moveto(piece, x, y)
+    # coords = canvas.coords(piece)
+    # x = (coords[0] // 100) * 100
+    # y = (coords[1] // 100) * 100
+    # canvas.moveto(piece, x, y)
+    pass
 
 # Binds:
 canvas.bind("<ButtonPress-1>", on_start)
 canvas.bind("<B1-Motion>", move)
 canvas.bind("<ButtonRelease-1>", on_drop)
 
+#Testing FEN functionality
+starting_position_fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+set_up_from_FEN(starting_position_fen)
 
 window.mainloop()
